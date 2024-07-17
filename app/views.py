ad7803aa84d0b78c7b40a014b917030ab67c35b4
Paul <paulhoratau@gmail.com>
2024-07-17 13:17:13 +0300

    added login/logout functionality

--- app/views.py
+++ app/views.py
@@ -5,7 +5,11 @@ from .forms import CreateUserForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
-
+from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -18,7 +22,29 @@ def register(request):
             form.save()
             user = form.cleaned_data.get('username')
             messages.success(request, 'Account was created for ' + user)
+            return redirect('login')
     context = {'form': form}
     return render(request, 'novanexus/register.html', context)
 
 
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                auth_login(request, user)
+            else:
+                messages.info(request, 'Username or password is incorrect!')
+                return render(request, 'novanexus/login.html', {'form': form})
+        else:
+            return HttpResponse("Your data is not valid")
+    else:
+        form = AuthenticationForm()
+    return render(request, 'novanexus/login.html', {'form': form})
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
